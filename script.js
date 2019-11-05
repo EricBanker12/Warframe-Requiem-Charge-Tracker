f408c776bb050e44d7aaf0c358d1f97cf6490641
ericbanker12 <ericbanker12@gmail.com>
2019-11-04 19:40:11 -0600

    add images, adjust script for readability, adjust styling and simplify media queries

--- script.js
+++ script.js
@@ -5,33 +5,11 @@ let state = {
     XATA: 0,
     FASS: 0,
     RIS: 0,
-    KRAH: 0,
+    KHRA: 0,
     NETRA: 0,
 }
 
-function clickHandler(event) {
-    // console.log('click')
-
-    const button = event.target.closest('button')
-    
-    if (!button) return
-
-    const command = button.textContent
-    const counter = button.closest('.counter-container').children[0]
-
-    // console.log('click button', command)
-
-    if (command == '-1') state[counter.id] -= 1
-    if (command == '+1') state[counter.id] += 1
-    if (command == '+3') state[counter.id] += 3
-    
-    counter.textContent = String(state[counter.id])
-    localStorage.setItem('state', JSON.stringify(state))
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    // console.log('DOM loaded')
-
+function DOMLoadedHandler() {
     // get data from browser localStorage cache
     const data = localStorage.getItem('state')
     if (data) state = JSON.parse(data)
@@ -41,6 +19,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // set counters to state
     for (let name of Object.keys(state)) {
-        document.getElementById(name).firstChild.textContent = String(state[name])
+        document.getElementById(name).textContent = String(state[name])
     }
-})
+}
+
+function clickHandler(event) {
+    // get clicked button
+    const button = event.target.closest('button')
+    
+    if (!button) return
+
+    // get button text and counter element
+    const command = button.textContent
+    const counter = button.closest('.counter-container').children[0]
+
+    // change state and update counter
+    if (command == '-1') state[counter.id] -= 1
+    if (command == '+1') state[counter.id] += 1
+    if (command == '+3') state[counter.id] += 3
+    
+    counter.textContent = String(state[counter.id])
+
+    // save new state to browser cache
+    localStorage.setItem('state', JSON.stringify(state))
+}
+
+document.addEventListener('DOMContentLoaded', DOMLoadedHandler)
